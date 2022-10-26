8872c9b2c9f3f57d459356e786dc434be06d7370
pseiasDesktop <oseiasfilho99@gmai.com>
2022-10-26 16:20:09 -0300

    startDaniProject

---
+++ src/main/java/com/projetoDani/projetoDani/ProjetoDaniApplication.java
@@ -0,0 +1,13 @@
+package com.projetoDani.projetoDani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoDani/projetoDani/ProjetoDaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoDani.projetoDani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
